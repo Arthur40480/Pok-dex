a29039fb62cf7105d08ee3c10d0126188999d379
Arthur40480 <arthurgibert@free.fr>
2023-06-13 17:19:26 +0200

    Add input event

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 
-    
+// On récupère les éléments du DOM
+const searchInputElt = document.querySelector('.recherche-pokemon input')
+
+// Ajout d'évènement sur l'input
+searchInputElt.addEventListener('input', function(e) {
+    if(e.target.value !== "") {
+       e.target.parentNode.classList.add('active-input');
+    }else if(e.target.value === "") {
+        e.target.parentNode.classList.remove('active-input');
+    }
+})
+
 function addPokemon() {
     fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
     .then(response => response.json())
