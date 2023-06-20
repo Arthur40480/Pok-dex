49736815b1b1f3f0f12b003fe20791bb83c086f9
Arthur40480 <arthurgibert@free.fr>
2023-06-20 17:30:32 +0200

    Add func to retrieve french pokemon name

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 // On récupère les éléments du DOM
-const searchInputElt = document.querySelector('.recherche-pokemon input')
+const searchInputElt = document.querySelector('.recherche-pokemon input');
+const pokemonContainerElt = document.querySelector('.pokemon-container');
+
 
 // Ajout d'évènement sur l'input
 searchInputElt.addEventListener('input', function(e) {
@@ -11,12 +13,44 @@ searchInputElt.addEventListener('input', function(e) {
     }
 })
 
-function addPokemon() {
+// Fonction permettant de récupérer l'url des pokémons
+function fetchDataPokemonUrl() {
     fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        data.results.forEach(pokemonUrl => {
+            fetchPokemonData(pokemonUrl);
+        });
     });
 }
 
-addPokemon();
+// Fonction qui récupère les données des pokémons via leurs url
+function fetchPokemonData(pokemon) {
+    const pokemonList = []
+    const pokemonUrl = pokemon.url;
+    
+    fetch(pokemonUrl)
+    .then(response => response.json())
+    .then(data => {
+        const pokemonFullData = {};
+        pokemonFullData.id = data.id;
+        pokemonFullData.type = data.types[0].type.name;
+        pokemonFullData.img = data.sprites.front_default;
+        pokemonFullData.name = fetchFrenchPokemonName(pokemonFullData);
+        pokemonList.push(pokemonFullData);    
+    })
+    console.log(pokemonList);
+}
+
+// Fonction qui récupère le nom français du pokemon via son id
+function fetchFrenchPokemonName(pokemon) {
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon.id}/`)
+    .then(response => response.json())
+    .then(data => {
+        pokemon.name = data.names.find(name => name.language.name === "fr").name;
+    })
+};
+
+
+fetchDataPokemonUrl();
+
