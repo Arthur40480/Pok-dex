921ef70c33f9be532aff82bb726a4841284d22e1
Arthur40480 <arthurgibert@free.fr>
2023-06-22 15:23:48 +0200

    Add Pokemon colors

---
+++ colors.js
@@ -0,0 +1,19 @@
+const typeColors = [
+    { type: "water", traduction: "eau", color: "#0674BA" },
+    { type: "grass", traduction: "plante", color: "#119845" },
+    { type: "fire", traduction: "feu", color: "#BC1F28" },
+    { type: "bug", traduction: "insecte", color: "#119845" },
+    { type: "normal", traduction: "normal", color: "#BEB3B0" },
+    { type: "poison", traduction: "poison", color: "#7B428F" },
+    { type: "electric", traduction: "electrique", color: "#E7FF13" },
+    { type: "ground", traduction: "sol", color: "#A83A22" },
+    { type: "fairy", traduction: "fée", color: "#D44577" },
+    { type: "rock", traduction: "roche", color: "#A83A22" },
+    { type: "psychic", traduction: "psy", color: "#7B428F" },
+    { type: "ice", traduction: "glace", color: "#0674BA"},
+    { type: "fighting", traduction: "combat", color: "#A83A22"},
+    { type: "dragon", traduction: "dragon", color: "#AD8631" },
+    { type: "ghost", traduction: "fantôme", color: "#7B428F" }
+];
+
+export { typeColors };
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Importation de mon tableau de couleur
+import { typeColors } from "./colors.js";
+
 // On récupère les éléments du DOM
 const searchInputElt = document.querySelector('.recherche-pokemon input');
 const pokemonContainerElt = document.querySelector('.pokemon-container');
@@ -9,16 +12,16 @@ searchInputElt.addEventListener('input', function(e) {
     }else if(e.target.value === "") {
         e.target.parentNode.classList.remove('active-input');
     }
-})
+});
 
 // Fonction permettant de récupérer les données de chaque pokémon
-function fetchAllPokemonData() {
+async function fetchAllPokemonData() {
     const pokemonList = [];
 
     for (let i = 1; i <= 151; i++) {    
         const pokemonData = {};
 
-        fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+        await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`)
         .then(response => response.json())
         .then(data => {
             pokemonData.id =  data.id;
@@ -33,11 +36,46 @@ function fetchAllPokemonData() {
             pokemonList.push(pokemonData);
             pokemonList.sort((a, b) => a.id - b.id);
         });
-    }
-    console.log(pokemonList);
-}
+    };
+    displayPokemon(pokemonList);
+};
+
+// Fonction permettant de créer les éléments html pour afficher les pokémons
+function displayPokemon(pokemonList) {
+    pokemonList.forEach(pokemon => {
+        const cardPokemonElt = document.createElement('div');
+        cardPokemonElt.classList.add('pokemon-card');
+        cardPokemonElt.classList.add(pokemon.type);
+        const cardColor = typeColors.find(type => type.type === pokemon.type).color;
+        const typeTraduction = typeColors.find(type => type.type === pokemon.type).traduction;
+        cardPokemonElt.style.border = `5px solid ${cardColor}`;
+        cardPokemonElt.style.boxShadow = `2px 2px 20px ${cardColor}`;
+        
+        
+        const imgPokemonElt = document.createElement('img');
+        imgPokemonElt.classList.add('img-pokemon');
+        imgPokemonElt.src = pokemon.img;
+        
+        const informationContainerElt = document.createElement('div');
+        informationContainerElt.classList.add('container-information');
+
+        const pokemonIdElt = document.createElement('p');
+        pokemonIdElt.classList.add('pokemon-id');
+        pokemonIdElt.innerText = '#' + pokemon.id;
+
+        const pokemonNameElt = document.createElement('p');
+        pokemonNameElt.classList.add('pokemon-name');
+        pokemonNameElt.innerText = pokemon.name;
+
+        const pokemonTypeElt = document.createElement('p');
+        pokemonTypeElt.classList.add('pokemon-type');
+        pokemonTypeElt.innerText = `${pokemon.type} || ${typeTraduction}`;
+        
+        informationContainerElt.append(pokemonNameElt, pokemonTypeElt);
+        cardPokemonElt.append(pokemonIdElt, imgPokemonElt, informationContainerElt);
+        pokemonContainerElt.append(cardPokemonElt);
+    });
+};
 
 fetchAllPokemonData();
-
-
 
